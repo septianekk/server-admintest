cd4dd530d29d3da094c72c2519a2281b694d4892
sepitanekk <septianekokuncahyono@gmail.com>
2021-04-05 03:52:58 +0700

    update user

--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,7 @@ const userController = require("../controllers/userController");
 
 router.post("/regis", userController.regisUser);
 router.post("/login", userController.loginUser);
+router.put("/user/profile/update", userController.updateProfile);
+router.get("/user/profile/:id", userController.getUserProfile);
 
 module.exports = router;
