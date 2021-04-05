1283d917cdc2fed391acba8c527f9908325c999a
sepitanekk <septianekokuncahyono@gmail.com>
2021-04-05 07:54:29 +0700

    order-pain

--- routes/order.js
+++ routes/order.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const orderController = require("../controllers/orderController");
+const protect = require("../middlewares/auth");
 
-router.post("/order/new", orderController.addOrderItems);
-router.get("/order/get/:id", orderController.getOrderById);
+router.post("/order/new", protect, orderController.addOrderItems);
+router.get("/order/get/:id", protect, orderController.getOrderById);
+router.put("/order/:id/pay", protect, orderController.updateOrderToPaid);
 
 module.exports = router;
